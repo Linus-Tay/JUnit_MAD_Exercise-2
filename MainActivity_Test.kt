f64effba2436b12b01c35def2b4cd1f1cdb98781
Tiang Ann Linus Tay <s10195119@connect.np.edu.sg>
2021-11-29 17:18:42 +0800

    Test

--- MainActivity_Test.kt
+++ MainActivity_Test.kt
@@ -24,4 +24,11 @@ class MainActivityTest {
         onView(withId(R.id.btnFollow)).perform(click())
         onView(withId(R.id.btnFollow)).check(matches(withText("Unfollow")));
     }
+
+    @Test
+    fun failTest() {
+        val activityScenario = launch(MainActivity::class.java)
+        onView(withId(R.id.Follow)).perform(click())
+        onView(withId(R.id.Follow)).check(matches(withText("Unfollow")));
+    }
 }
